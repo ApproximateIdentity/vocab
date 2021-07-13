05f94d6c6d35b818e3c415db327949b51f0fb9cd
Thomas Nyerg <twn@thomasnyberg.com>
2021-07-13 09:06:55 +0200

    Add in news deck

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all: myanki
 
 csv:
-	libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 --outdir . words.ods
+	libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 --outdir . news.ods
 
 myanki: csv
-	python3 csv_to_anki.py > words.myanki
+	python3 csv_to_anki.py > news.myanki
+
+clean:
+	rm -fv *.csv *.myanki
--- csv_to_anki.py
+++ csv_to_anki.py
@@ -7,7 +7,7 @@ def formatter(dict_reader):
         # Initial data cleaning
         row = {key: val.strip() for key, val in row.items()}
         # Sanity checks
-        assert row['type'] in ['adj', 'adv', 'noun', 'verb', 'phrase', 'prep', 'conj', 'pron', 'comp', 'interj']
+        assert row['type'] in ['adj', 'adv', 'noun', 'verb', 'phrase', 'prep', 'conj', 'pron', 'comp', 'interj', 'sup']
         # Formatting
         for first_lang in ['english', 'czech']:
             second_lang = 'czech' if first_lang == 'english' else 'english'
@@ -33,10 +33,10 @@ def formatter(dict_reader):
                 if perfective:
                     perfective = "." + perfective
                 yield f"{row[first_lang]} (v{perfective});{row[second_lang]}"
-            elif row['type'] in ['phrase', 'comp', 'interj']:
+            elif row['type'] in ['phrase', 'comp', 'interj', 'sup']:
                 yield f"{row[first_lang]};{row[second_lang]}"
             else:
                 raise NotImplementedError(f"row type: {row['type']}")
 
-for row in formatter(csv.DictReader(open('words.csv'))):
+for row in formatter(csv.DictReader(open('news.csv'))):
     print(row)
